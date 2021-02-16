3c932b9ac9659474f7c22054f028d59bea58816a
Chen-Hailin <chenhailin2@gmail.com>
2021-02-16 17:00:54 +0800

    bug fix

--- word_language_model/main.py
+++ word_language_model/main.py
@@ -74,8 +74,8 @@ device = torch.device("cuda" if args.cuda else "cpu")
 ###############################################################################
 
 corpus = data.Corpus(args.data)
-print(len(corpus.dictionary))
-exit()
+
+
 # Starting from sequential data, batchify arranges the dataset into columns.
 # For instance, with the alphabet as the sequence and batch size 4, we'd get
 # ┌ a g m s ┐
